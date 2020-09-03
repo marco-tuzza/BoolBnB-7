215d07d213198a59029e25a4644a98f09f71c681
Marco <marco.faltoni@outlook.it>
2020-09-03 17:45:29 +0200

    Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,8 +39,6 @@
                                                     <li> <a href="{{ route('login') }}">Accedi</a> </li>
                                                     <li> <a href="{{ route('register') }}">Registrati</a> </li>
                                             @endauth
-                                            
-                                            
                                         </ul>
                                     </div>
                                 </div>
@@ -165,18 +163,3 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
-
-
-@if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-            <a href="{{ url('/home') }}">Home</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif
