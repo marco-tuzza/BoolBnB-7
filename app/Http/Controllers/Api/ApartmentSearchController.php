15e3031211c3aca90ff3adcb3fea6f7b86f3a353
Marco Tuzza <marcotuzza@hotmail.it>
2020-09-10 15:03:49 +0200

    filtro funzionante lato php

--- app/Http/Controllers/Api/ApartmentSearchController.php
+++ app/Http/Controllers/Api/ApartmentSearchController.php
@@ -5,27 +5,44 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Apartment;
+use App\Service;
 
 class ApartmentSearchController extends Controller
 {
     public function index() {
         $appartamenti = Apartment::all();
+        $array = [];
+        $arrayServizi = [];
+        foreach ($appartamenti as $appartamento) {
+            $servizio = $appartamento->services;
+            array_push($arrayServizi, $servizio);
+            array_push($array, $appartamento);
+        };
         return response()->json([
             'success' => true,
-            'data' => $appartamenti,
+            'data' => [
+                'appartamento' => $array,
+                'servizi' => $arrayServizi
+            ],
             'count' => $appartamenti->count()
         ]);
     }
 
     public function search($lat, $lon) {
         $array = [];
+        $arrayServizi = [];
         $appartamenti = Apartment::all()->where('latitudine', '>', $lat - 0.5)->where('latitudine', '<', $lat + 0.5)->where('longitudine', '>', $lon - 0.5)->where('longitudine', '<', $lon + 0.5);
         foreach ($appartamenti as $appartamento) {
+            $servizio = $appartamento->services;
+            array_push($arrayServizi, $servizio);
             array_push($array, $appartamento);
         };
         return response()->json([
             'success' => true,
-            'data' => $array,
+            'data' => [
+                'appartamento' => $array,
+                'servizi' => $arrayServizi
+            ],
             'count' => $appartamenti->count()
         ]);
     }
